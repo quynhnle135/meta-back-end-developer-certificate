2ff7bc75c63110695cd066b5799c641fc2d9c46a
Quinn Le <quynhnle135@gmail.com>
2023-06-27 13:24:35 -0400

    Learned about Foreign Key in Django Models

--- Django-Web-Framework/buildDjango/myproject/myapp/admin.py
+++ Django-Web-Framework/buildDjango/myproject/myapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Drinks
+from .models import Menu, MenuCategory, Drinks, DrinksCategory
 
 # Register your models here.
+# admin.site.register(Drinks)
+admin.site.register(Menu)
+admin.site.register(MenuCategory)
 admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
--- Django-Web-Framework/buildDjango/myproject/myapp/models.py
+++ Django-Web-Framework/buildDjango/myproject/myapp/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
 
-class Menuitems(models.Model):
-    itemname = models.CharField(max_length=200)
-    category = models.CharField(max_length=300)
-    year = models.IntegerField()
+# class Menuitems(models.Model):
+#     itemname = models.CharField(max_length=200)
+#     category = models.CharField(max_length=300)
+#     year = models.IntegerField()
 
+# class Drinks(models.Model):
+#     drink_name = models.CharField(max_length=200)
+#     price = models.IntegerField()
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
+
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+    
 class Drinks(models.Model):
-    drink_name = models.CharField(max_length=200)
+    drink = models.CharField(max_length=200)
     price = models.IntegerField()
+    category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
+
