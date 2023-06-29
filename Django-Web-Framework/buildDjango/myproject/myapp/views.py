6c7105c52cb97f6359e009f93238e8eac66ac6e1
Quinn Le <quynhnle135@gmail.com>
2023-06-29 14:24:59 -0400

    Exercise: Creating templates

--- Django-Web-Framework/buildDjango/myproject/myapp/views.py
+++ Django-Web-Framework/buildDjango/myproject/myapp/views.py
@@ -6,13 +6,19 @@ def home(request):
     return HttpResponse('Welcome to Little Lemon!')
 
 def about(request):
-    return HttpResponse('About us')
+    # return HttpResponse('About us')
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, "about.html", about_content)
+
+def menu(request):
+    menu_content = {'menu': 'Little Lemon menu'}
+    return render(request, "menu.html", menu_content)
 
 def book(request):
     return HttpResponse('Make a booking')
 
-def menu(request):
-    return HttpResponse('Menu for Little Lemon')
+# def menu(request):
+#     return HttpResponse('Menu for Little Lemon')
 
 def form_view(request):
     form =  LogForm()
