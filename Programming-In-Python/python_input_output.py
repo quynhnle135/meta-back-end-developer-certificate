a166a21d8b120e705097546c378c90aaa4ad05e8
Quinn Le <quynhnle135@gmail.com>
2023-07-03 13:32:54 -0400

    Learned about Python's input, output, and typecasting

---
+++ Programming-In-Python/python_input_output.py
@@ -0,0 +1,15 @@
+print("My favorite fruits are {0} and {1}".format("Lychee", "Banana"))
+
+first_fav_fruit = "Lychee"
+second_fav_fruit = "Banana"
+print(f"My favorite fruits are {first_fav_fruit} and {second_fav_fruit}")
+
+user_name = input("Please enter your name: ")
+user_year = input("Please enter your birth year: ")
+
+print(f"Hi, {user_name}! You are {str(2023 - int(user_year))} years old")
+
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the second number: "))
+
+print(f"Sum of {num_1} and {num_2}: {str(num_1 + num_2)}")
---
+++ Programming-In-Python/python_type_casting.py
@@ -0,0 +1,8 @@
+x = 11
+print(type(x))
+x = str(x)
+print(type(x))
+
+print(11 == 11.0)
+print(type(10 + 10.0))
+print(10 + 10.0)
