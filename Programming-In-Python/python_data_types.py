b553e1b0ae593a7df0fd3bb785579dd1e2af3c8f
Quinn Le <quynhnle135@gmail.com>
2023-07-03 13:05:03 -0400

    Learned about Python's data tyoes and string

---
+++ Programming-In-Python/python_data_types.py
@@ -0,0 +1,12 @@
+a = 10.0
+b = 10
+print(type(a))
+print(type(b))
+
+my_list = [1, 2, 3, 4]
+my_tupple = (1, 2, 3, 4)
+my_dictionary = {"My crush": "Johnathan"}
+
+print(type(my_list))
+print(type(my_tupple))
+print(type(my_dictionary))
---
+++ Programming-In-Python/python_string.py
@@ -0,0 +1,23 @@
+my_string = "Hello World"
+
+# get the first character
+print(my_string[0])
+
+# get the last character
+print(my_string[-1])
+
+# get the second to last character
+print(my_string[-2])
+
+# string comprehension
+print(my_string[::2])
+
+# concatenation
+my_name = "Quinn"
+print(f"Hi, my name is {my_name}")
+
+print("Hi, my name is " + my_name)
+
+print("Hi, my name is", my_name)
+
+print(f"The length of my name: {len(my_name)}")
