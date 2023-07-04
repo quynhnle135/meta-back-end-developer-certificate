5e53436d360bdc49d0c60f8fd47399398a81ac62
Quinn Le <quynhnle135@gmail.com>
2023-07-04 13:45:55 -0400

    Practice printing keys and values in dictionary

--- Programming-In-Python/draft.py
+++ Programming-In-Python/draft.py
@@ -1,1 +1,16 @@
-print("Hello Python")
+menu = {
+    1: {"name": 'espresso',
+        "price": 1.99},
+    2: {"name": 'coffee',
+        "price": 2.50},
+    3: {"name": 'cake',
+        "price": 2.79},
+    4: {"name": 'soup',
+        "price": 4.50},
+    5: {"name": 'sandwich',
+        "price": 4.99}
+}
+
+
+for item in menu.values():
+    print(item['price'])
