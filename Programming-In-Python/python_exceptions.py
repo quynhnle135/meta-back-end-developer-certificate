dca5448ba56a6df8c0e9d155b2d64f70865a78c1
Quinn Le <quynhnle135@gmail.com>
2023-07-04 15:40:53 -0400

    Learned about exceptions in Python

---
+++ Programming-In-Python/python_exceptions.py
@@ -0,0 +1,18 @@
+def divide_by(a, b):
+    return a / b
+
+
+try:
+    ans = divide_by(40, 0)
+except ZeroDivisionError as e:
+    print(e, "we cannot divide by zero")
+except Exception as e:
+    print(e, "something went wrong")
+
+# index error
+items = [1, 2, 3, 4, 5]
+try:
+    item = items[6]
+    print(item)
+except:
+    print("The index does not exist in the list")
