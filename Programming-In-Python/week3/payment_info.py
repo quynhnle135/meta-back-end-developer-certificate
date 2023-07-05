dfed4e9db88e3ced2bf99130de13dc12ba575305
Quinn Le <quynhnle135@gmail.com>
2023-07-05 12:53:19 -0400

    Instance method exercise

---
+++ Programming-In-Python/week3/payment_info.py
@@ -0,0 +1,21 @@
+class Payslips():
+    def __init__(self, name, payment, amount) -> None:
+        self.name = name
+        self.payment = payment
+        self.amount = amount
+
+    def pay(self):
+        self.payment = "yes"
+
+    def status(self):
+        if self.payment == "yes":
+            return self.name + " is paid " + str(self.amount)
+        else:
+            return self.name + " is not paid yet "
+
+
+johnathan = Payslips("Johnathan", "no", 1000)
+print(johnathan.status())
+
+johnathan.pay()
+print(johnathan.status())
