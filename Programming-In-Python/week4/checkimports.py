00bf9e2a9e0ac8f18fc62a9b6b428e43520fa246
Quinn Le <quynhnle135@gmail.com>
2023-07-06 12:24:57 -0400

    Learned about models, scope, and namespacing in Python

---
+++ Programming-In-Python/week4/checkimports.py
@@ -0,0 +1,7 @@
+import trial
+import sys
+
+sys.path.insert(
+    1, r'/Users/quinnle/Desktop/Meta-Back-End-Developer/Programming-In-Python/week4/workplace')
+
+print(trial.names)
---
+++ Programming-In-Python/week4/imports.py
@@ -0,0 +1,13 @@
+# # import math
+
+# from math import sqrt
+
+# # root = math.sqrt(9)
+# root = sqrt(9)
+
+# print(root)
+
+from math import factorial as f
+
+result = f(5)
+print(result)
