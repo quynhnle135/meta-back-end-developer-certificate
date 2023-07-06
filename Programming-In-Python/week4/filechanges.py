c6ef6b2b3f8e33dc62772c3e837fdcfe9e66ff27
Quinn Le <quynhnle135@gmail.com>
2023-07-06 14:06:01 -0400

    Learned about modules in Python

---
+++ Programming-In-Python/week4/filechanges.py
@@ -0,0 +1,5 @@
+import os
+
+contents = os.listdir(
+    r'/Users/quinnle/Desktop/Meta-Back-End-Developer/Programming-In-Python/week4')
+print(contents)
---
+++ Programming-In-Python/week4/reloads.py
@@ -0,0 +1,10 @@
+# import importlib
+# import sample
+
+
+# importlib.reload(sample)
+# importlib.reload(sample)
+# importlib.reload(sample)
+
+import importlib
+import filechanges
