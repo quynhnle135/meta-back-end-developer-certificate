ee2e411f9956fac7a74c0a3744c183e134cbb3b1
Quinn Le <quynhnle135@gmail.com>
2023-07-07 15:25:12 -0400

    Did exercise about functions, loops, and data structures

--- Programming-In-Python/draft.py
+++ Programming-In-Python/draft.py
@@ -1,3 +1,4 @@
+from math import pi
 menu = {
     1: {"name": 'espresso',
         "price": 1.99},
@@ -14,3 +15,19 @@ menu = {
 
 for item in menu.values():
     print(item['price'])
+
+
+print(pi)
+
+names = ["Anna", "Natasha", "Mike"]
+names.insert(2, "Xi")
+print(names)
+
+
+sample_dict = {1: 'c', 2: 'T', 3: 'J'}
+
+for x in sample_dict:
+    print(x)
+
+for x in range(1, 4):
+    print(int(str(float(x))))
